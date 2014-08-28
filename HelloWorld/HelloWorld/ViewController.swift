86233066f6d4f43434f83083bda7b489e662293c
jlink <jessie.link@livingsocial.com>
2014-08-28 15:08:37 -0400

    Initial Commit

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        searchItunesFor("Disney");
+        getLSFeed()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,38 +26,32 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
         return tableData.count;
     }
-    
+
     func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
         
         var rowData: NSDictionary = self.tableData[indexPath.row] as NSDictionary
         
-        cell.text = rowData["trackName"] as String
+        cell.text = rowData["desc"] as String
         
-        // Grab the artworkUrl60 key to get an image URL for the app's thumbnail
-        var urlString: NSString = rowData["artworkUrl60"] as NSString
+        // Grab the src key to get an image URL for the app's thumbnail
+        var urlString: NSString = rowData["src"] as NSString
         var imgURL: NSURL = NSURL(string: urlString)
         
         // Download an NSData representation of the image at the URL
         var imgData: NSData = NSData(contentsOfURL: imgURL)
         cell.image = UIImage(data: imgData)
         
-        // Get the formatted price string for display in the subtitle
-        var formattedPrice: NSString = rowData["formattedPrice"] as NSString
+        // Get the deal details for display in the subtitle
+        var formattedPrice: NSString = rowData["attrib"] as NSString
         
         cell.detailTextLabel.text = formattedPrice
         
         return cell
     }
-    
-    func searchItunesFor(searchTerm: String) {
-        
-        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
-        var itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
-        
-        // Now escape anything else that isn't URL-friendly
-        var escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
-        var urlPath = "https://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
+
+    func getLSFeed(){
+        var urlPath = "http://sheltered-bastion-2512.herokuapp.com/feed.json"
         var url: NSURL = NSURL(string: urlPath)
         var session = NSURLSession.sharedSession()
         var task = session.dataTaskWithURL(url, completionHandler: {data, response, error -> Void in
@@ -67,24 +61,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 println(error.localizedDescription)
             }
             var err: NSError?
-            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSArray
             if(err?) {
                 // If there is an error parsing JSON, print it to the console
                 println("JSON Error \(err!.localizedDescription)")
             }
-            var results: NSArray = jsonResult["results"] as NSArray
             dispatch_async(dispatch_get_main_queue(), {
-                self.tableData = results
+                self.tableData = jsonResult
                 self.appsTableView.reloadData()
                 })
             })
         task.resume()
-    }
-    
-    func getLSFeed(){
-        var urlPath = "http://sheltered-bastion-2512.herokuapp.com/feed.json"
-        var url: NSURL = NSURL(string: urlPath)
-        var session = NSURLSession.sharedSession()
     }
 
 }
